b73b665025815642666d113d109689eacad97140
Tomyred <tomy.rojo.1@gmail.com>
2022-02-16 20:03:00 -0300

    connection string removed

--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,7 @@ const config = {
     BaseDomain: process.env.BACKEND_BASE_DOMAIN
         ? process.env.BACKEND_BASE_DOMAIN
         : "http://localhost:8080",
-    MongoDbConnection: process.env.BACKEND_DB_CONNECTION
-        ? // process.env.BACKEND_DB_CONNECTION : "mongodb+srv://senado-azure:senado@1@cluster0-ecned.azure.mongodb.net/desarrollo",
-          process.env.BACKEND_DB_CONNECTION
-        : "mongodb+srv://Tred:charlyelaguantesucks@ruta20db.nmffy.mongodb.net/Ruta20DB?retryWrites=true&w=majority",
+    MongoDbConnection: process.env.BACKEND_DB_CONNECTION,
 };
 
 export default config;
