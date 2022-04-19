39faae4f88118dc1efa7661e914ffc4de7a4d8a6
Tomyred <tomy.rojo.1@gmail.com>
2022-04-19 11:44:25 -0300

    edit classroom course day

--- src/routes/v1/classroom/schedule.js
+++ src/routes/v1/classroom/schedule.js
@@ -6,92 +6,137 @@ import { defaultResponse } from "../../../utils.js";
 const scheduleRouter = Router();
 
 scheduleRouter.put("/:id", async (req, res) => {
-    const id = req.params.id;
-    const data = req.body;
-    if (ObjectId.isValid(id) === false) {
-        const error = { message: `"${id}" is not a valid id` };
+    try {
+        const id = req.params.id;
+        const data = req.body;
+        if (ObjectId.isValid(id) === false) {
+            const error = { message: `"${id}" is not a valid id` };
+            defaultResponse(req, res, null, error);
+        }
+
+        const document = await classroomModel.findOne({ _id: id });
+
+        const element = document.schedule.find(element => {
+            return element.day === data.day;
+        });
+        if (!element) {
+            const updated = await classroomModel.findOneAndUpdate(
+                { _id: id },
+                { $push: { schedule: data } }
+            );
+            defaultResponse(req, res, updated);
+        } else {
+            const updated = await classroomModel.findOneAndUpdate(
+                { _id: id },
+                { $push: { "schedule.$[elemX].courses": data.courses[0] } },
+                {
+                    arrayFilters: [
+                        {
+                            "elemX.day": data.day,
+                        },
+                    ],
+                }
+            );
+
+            defaultResponse(req, res, updated);
+        }
+    } catch (error) {
         defaultResponse(req, res, null, error);
     }
+});
 
-    const document = await classroomModel.findOne({ _id: id });
+scheduleRouter.put("/remove/:id/:day", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const day = req.params.day;
+        const data = req.body;
 
-    const element = document.schedule.find(element => {
-        return element.day === data.day;
-    });
-    if (!element) {
+        if (ObjectId.isValid(id) === false) {
+            const error = { message: `"${id}" is not a valid id` };
+            defaultResponse(req, res, null, error);
+        }
+
         const updated = await classroomModel.findOneAndUpdate(
             { _id: id },
-            { $push: { schedule: data } }
-        );
-        defaultResponse(req, res, updated);
-    } else {
-        const updated = await classroomModel.findOneAndUpdate(
-            { _id: id },
-            { $push: { "schedule.$[elemX].courses": data.courses[0] } },
+            { $pull: { "schedule.$[elemX].courses": data } },
             {
                 arrayFilters: [
                     {
-                        "elemX.day": data.day,
+                        "elemX.day": day,
                     },
                 ],
             }
         );
 
         defaultResponse(req, res, updated);
-    }
-});
-
-scheduleRouter.put("/remove/:id/:day", async (req, res) => {
-    const id = req.params.id;
-    const day = req.params.day;
-    const data = req.body;
-
-    if (ObjectId.isValid(id) === false) {
-        const error = { message: `"${id}" is not a valid id` };
+    } catch (error) {
         defaultResponse(req, res, null, error);
     }
-
-    const updated = await classroomModel.findOneAndUpdate(
-        { _id: id },
-        { $pull: { "schedule.$[elemX].courses": data } },
-        {
-            arrayFilters: [
-                {
-                    "elemX.day": day,
-                },
-            ],
-        }
-    );
-
-    defaultResponse(req, res, updated);
 });
 
 scheduleRouter.put("/update/:id/:day", async (req, res) => {
-    const id = req.params.id;
-    const day = req.params.day;
-    const data = req.body;
+    try {
+        const id = req.params.id;
+        const prevDay = req.params.day;
+        const { hour, name, description, hosts, _id, day } = req.body;
+        const editedCourse = { hour, name, description, hosts };
 
-    if (ObjectId.isValid(id) === false) {
-        const error = { message: `"${id}" is not a valid id` };
+        if (ObjectId.isValid(id) === false) {
+            const error = { message: `"${id}" is not a valid id` };
+            defaultResponse(req, res, null, error);
+        }
+
+        if (prevDay === day) {
+            const updated = await classroomModel.findOneAndUpdate(
+                { _id: id },
+                {
+                    $set: {
+                        "schedule.$[elemX].courses.$[elemY]": {
+                            ...editedCourse,
+                            _id,
+                        },
+                    },
+                },
+                {
+                    arrayFilters: [
+                        {
+                            "elemX.day": day,
+                        },
+                        {
+                            "elemY._id": _id,
+                        },
+                    ],
+                }
+            );
+            defaultResponse(req, res, updated);
+        } else {
+            await classroomModel.findOneAndUpdate(
+                { _id: id },
+                { $pull: { "schedule.$[elemX].courses": editedCourse } },
+                {
+                    arrayFilters: [
+                        {
+                            "elemX.day": prevDay,
+                        },
+                    ],
+                }
+            );
+            const updated = await classroomModel.findOneAndUpdate(
+                { _id: id },
+                { $push: { "schedule.$[elemX].courses": editedCourse } },
+                {
+                    arrayFilters: [
+                        {
+                            "elemX.day": prevDay,
+                        },
+                    ],
+                }
+            );
+            defaultResponse(req, res, updated);
+        }
+    } catch (error) {
         defaultResponse(req, res, null, error);
     }
-    // res.json(data);
-    const updated = await classroomModel.findOneAndUpdate(
-        { _id: id },
-        { $set: { "schedule.$[elemX].courses.$[elemY]": data } },
-        {
-            arrayFilters: [
-                {
-                    "elemX.day": day,
-                },
-                {
-                    "elemY._id": data._id,
-                },
-            ],
-        }
-    );
-
-    defaultResponse(req, res, updated);
 });
 
 export default scheduleRouter;
