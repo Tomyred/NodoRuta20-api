3e4f046e190277e7a582d2f1078a257720bbcbad
Tomas Rojo <tomy.rojo.1@gmail.com>
2022-07-27 00:38:15 -0300

    fixed console route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import consolesRouter from "./v1/console/index.js";
 import radioStationRouter from "./v1/radioStation/index.js";
 
 export const register = app => {
-    app.use("/v1/links", consolesRouter);
+    app.use("/v1/consoles", consolesRouter);
     app.use("/v1/radioStation", radioStationRouter);
     app.use("/v1/classroom", classroomRouter);
 };
